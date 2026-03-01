83b309db0c0d82b128894d89207b3927f764f246
Ittipat Pattum <nampu@finema.co>
2026-03-01 17:01:45 +0700

    first save

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends HookConsumerWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
